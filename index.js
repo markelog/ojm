59e8b83c3f52e72eedcaba06925a09631ac30d2d
Oleg Gaidarenko <markelog@gmail.com>
2016-07-31 20:51:53 +0300

    Small adjustments

--- index.js
+++ index.js
@@ -23,25 +23,25 @@ url = url.replace('https://', '').replace('http://', '');
 got(`${provider}/${url}`).then(data => {
   if (data.body.includes("It's just you.")) {
     console.log();
-    console.log(` ${figures.tick}  "${url}" is up!`);
+    console.log(` ${figures.tick} "${url}" is up!`);
     console.log();
     process.exit(0);
   }
 
   if (data.body.includes("It's not just you!")) {
     console.error();
-    console.error(` ${figures.cross}  it's not just you! "${url}" is down`);
+    console.error(` ${figures.cross} it's not just you! "${url}" is down`);
     console.error();
     process.exit(1);
   }
 
   console.error();
-  console.error(` ${figures.cross}  huh? "http://${url}" doesn't look like a site on the interwho`);
+  console.error(` ${figures.cross} huh? "${url}" doesn't look like a site on the interwho`);
   console.error();
   process.exit(1);
 }).catch(() => {
   console.error();
-  console.error(` ${figures.cross}  can't connect, check your internet connection`);
+  console.error(` ${figures.cross} can't connect, check your internet connection`);
   console.error();
   process.exit(1);
 });
